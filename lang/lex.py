57fc1c5d4fbbf91f86529caf1746954d72e4abe4
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2024-02-03 09:30:17 -0800

    lexing numbers

--- lang/lex.py
+++ lang/lex.py
@@ -1,11 +1,12 @@
 import re
-from lang.tokens import Token
+from lang.tokens import Token, Literal
 from collections import deque
 
 
 class Re:
     line_number = re.compile("^\s*(\d+)(?:\s|)(.*)", flags=re.ASCII)
     whitespace = re.compile("\s", flags=re.ASCII)
+    digit = re.compile("\d", flags=re.ASCII)
 
 
 class BasicLexer:
@@ -34,6 +35,9 @@ class BasicLexer:
         if Re.whitespace.match(pk):
             return self.whitespace()
 
+        if Re.digit.match(pk):
+            return self.number()
+
         try:
             return self.chars.popleft()
         except IndexError:
@@ -51,6 +55,64 @@ class BasicLexer:
             except IndexError:
                 pass
             return Token.Whitespace(length)
+
+    def number(self):
+        s = str()
+        digits = 0
+        decimal = False
+        exp = False
+        while True:
+            try:
+                ch = self.chars.popleft()
+            except IndexError:
+                break
+            if ch == "e":
+                ch = "E"
+            if ch == "d":
+                ch = "D"
+            s += ch
+            if not exp and Re.digit.match(ch):
+                digits += 1
+            if ch == ".":
+                decimal = True
+            if ch == "D":
+                digits += 8
+            if ch == "!":
+                return Token.Literal(Literal.Single(s))
+            if ch == "#":
+                return Token.Literal(Literal.Double(s))
+            if ch == "%":
+                return Token.Literal(Literal.Integer(s))
+            try:
+                pk = self.chars[0]
+            except IndexError:
+                continue
+            if ch == "E" or ch == "D":
+                exp = True
+                if pk == "+" or pk == "-":
+                    continue
+                if not Re.digit.match(pk):
+                    exp = False
+                    s = s[:-1]
+                    self.chars.pushleft(ch)
+            if Re.digit.match(pk):
+                continue
+            if not exp and not decimal and pk == ".":
+                continue
+            if not exp and (pk == "E" or pk == "e" or pk == "D" or pk == "d"):
+                continue
+            if pk == "!" or pk == "#" or pk == "%":
+                continue
+            break
+        if digits > 7:
+            return Token.Literal(Literal.Double(s))
+        try:
+            i = int(s)
+            if i <= 32767 and i >= -32768 and str(i) == s:
+                return Token.Literal(Literal.Integer(s))
+        except:
+            pass
+        return Token.Literal(Literal.Single(s))
 
 
 def parse(source_line: str) -> (int, list[Token]):
