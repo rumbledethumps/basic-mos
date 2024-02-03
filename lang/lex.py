5a77a6ac9fab7eb6c0ec3ac615ec262a1535c405
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2024-02-03 08:40:01 -0800

    lexing spaces

--- lang/lex.py
+++ lang/lex.py
@@ -3,6 +3,11 @@ from lang.tokens import Token
 from collections import deque
 
 
+class Re:
+    line_number = re.compile("^\s*(\d+)(?:\s|)(.*)", flags=re.ASCII)
+    whitespace = re.compile("\s", flags=re.ASCII)
+
+
 class BasicLexer:
     def __init__(self, chars):
         self.chars = deque(chars)
@@ -15,7 +20,7 @@ class BasicLexer:
     def __next__(self):
         try:
             return self.pending.popleft()
-        except:
+        except IndexError:
             pass
 
         if self.remark:
@@ -23,23 +28,35 @@ class BasicLexer:
 
         try:
             pk = self.chars[0]
-        except:
+        except IndexError:
             pk = ""
 
-        if re.match("\s", pk, flags=re.ASCII):
-            self.chars.popleft()
-            return Token.Whitespace(2)
+        if Re.whitespace.match(pk):
+            return self.whitespace()
 
         try:
             return self.chars.popleft()
         except IndexError:
             raise StopIteration
 
+    def whitespace(self):
+        length = 0
+        while True:
+            self.chars.popleft()
+            length += 1
+
+            try:
+                if Re.whitespace.match(self.chars[0]):
+                    continue
+            except IndexError:
+                pass
+            return Token.Whitespace(length)
+
 
 def parse(source_line: str) -> (int, list[Token]):
     line_number = None
-    result = re.match("^\s*(\d+)(\s|)(.*)", source_line, flags=re.ASCII)
+    result = Re.line_number.match(source_line)
     if result:
         line_number = int(result.group(1))
-        source_line = result.group(3)
+        source_line = result.group(2)
     return (line_number, list(BasicLexer(source_line)))
